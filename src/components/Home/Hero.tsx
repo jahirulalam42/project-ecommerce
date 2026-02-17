ae6861213deb66a36048cd746de40f26664ef067
MD.Jahirul Alam Sarkar <jahirulalam41@gmail.com>
2026-02-17 15:55:27 +0600

    added responsive to the hero card

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -20,7 +20,7 @@ import Autoplay from "embla-carousel-autoplay";
 import HeroImage from "@/../public/images/hero-image.png";
 import Image from "next/image";
 import { Button } from "../ui/button";
-import { Search } from "lucide-react";
+import { CircleDollarSign, Globe, Recycle, Search, Truck } from "lucide-react";
 
 const Hero = () => {
   const plugin = React.useRef(
@@ -54,22 +54,22 @@ const Hero = () => {
     {
       title: "Free Shipping",
       description: "On orders over $50.00",
-      icon: "Test",
+      icon: <Truck strokeWidth={1.5} />,
     },
     {
       title: "Very easy to return",
       description: "Just phone number",
-      icon: "Test",
+      icon: <Recycle strokeWidth={1.5} />,
     },
     {
       title: "Worldwide delivery",
       description: "Fast delivery worldwide",
-      icon: "Test",
+      icon: <Globe strokeWidth={1.5} />,
     },
     {
       title: "Refunds policy",
       description: "60 days return for any reason",
-      icon: "Test",
+      icon: <CircleDollarSign strokeWidth={1.5} />,
     },
   ];
 
@@ -102,7 +102,7 @@ const Hero = () => {
                   <p className="text-gray-600 font-semibold text-lg">
                     Starting from $49.99
                   </p>
-                  <h2 className="text-black text-4xl md:text-6xl font-bold">
+                  <h2 className="text-black text-3xl md:text-4xl lg:text-6xl  font-bold">
                     Exclusive collection <br />
                     for everyone
                   </h2>
@@ -121,27 +121,39 @@ const Hero = () => {
       </div>
 
       {/* Card Section */}
-      <div className="h-[98px] bordered border-2 border-yellow-600 flex flex-row justify-between items-center">
-        {cardItems?.map((card: any, index: number) => {
-          return (
-            <div className="h-full flex flex-row items-center" key={index}>
-              {index !== 0 && (
-                <div className="w-0.5 h-1/2 bg-neutral-200 dark:bg-white/10 my-auto"></div>
-              )}
-              <div className="flex flex-row gap-2 md:gap-4 items-center px-4 lg:px-6 shrink-0">
-                <span>{card?.icon}</span>
-                <div className="flex flex-col md:gap-2">
-                  <h4 className="text-base md:text-lg font-semibold">
-                    {card?.title}
-                  </h4>
-                  <span className="hidden md:block text-xs lg:text-sm">
-                    {card?.description}
-                  </span>
-                </div>
+      <Card className="hidden lg:grid grid-cols-4">
+        {cardItems?.map((item, index) => (
+          <div
+            key={index}
+            className="relative w-full flex justify-center items-center"
+          >
+            {index > 0 && (
+              <div className="absolute left-0 h-full min-h-[1em] w-px self-stretch bg-black opacity-25 dark:via-neutral-400"></div>
+            )}
+            <div className="flex flex-row justify-center items-center gap-4">
+              <span className="font-bold">{item?.icon}</span>
+              <div className="flex flex-col gap-2">
+                <h1 className="font-bold xl:text-lg">{item?.title}</h1>
+                <p className="text-xs xl:text-sm text-gray-400">
+                  {item?.description}
+                </p>
               </div>
             </div>
-          );
-        })}
+          </div>
+        ))}
+      </Card>
+
+      <div className="lg:hidden flex flex-col gap-4">
+        <h1 className="font-bold text-lg">Nexton always with you</h1>
+
+        <div className="overflow-auto flex flex-row gap-6">
+          {cardItems?.map((item, index) => (
+            <div key={index} className="flex flex-row gap-2">
+              <span>{item?.icon}</span>
+              <h1 className="whitespace-nowrap">{item?.title}</h1>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
